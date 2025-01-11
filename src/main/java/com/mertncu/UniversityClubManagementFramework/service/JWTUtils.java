0268fa7dfd2323b65cbc9ccee400edb3ef9bdb4b
Yener Mert Uncu <86873433+mertncu@users.noreply.github.com>
2025-01-12 02:08:18 +0300

    Implement JWTUtils service for generating and validating JWT and refresh tokens

---
+++ src/main/java/com/mertncu/UniversityClubManagementFramework/service/JWTUtils.java
@@ -0,0 +1,66 @@
+package com.mertncu.UniversityClubManagementFramework.service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.function.Function;
+
+@Component
+public class JWTUtils {
+    private SecretKey secretKey;
+
+    private static final long JWT_EXPIRATION = 864000000;
+
+    public JWTUtils() {
+        String secretKeyString = "vXR7BxF8kFMh1yP9ZX4VbEXA1tGJeYm8f4kUpzyLJh69oHxM1QwMN7pZtHqduFbI";
+        byte[] decodedKey = Base64.getDecoder().decode(secretKeyString.getBytes(StandardCharsets.UTF_8));
+        this.secretKey = new SecretKeySpec(decodedKey, "HmacSHA256");
+    }
+
+    public String generateToken(UserDetails userDetails) {
+        return io.jsonwebtoken.Jwts.builder()
+                .subject(userDetails.getUsername())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + JWT_EXPIRATION))
+                .signWith(secretKey)
+                .compact();
+    }
+
+    public String generateRefreshToken(HashMap<String, Object> claims, UserDetails userDetails){
+        return io.jsonwebtoken.Jwts.builder()
+                .claims(claims)
+                .subject(userDetails.getUsername())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + JWT_EXPIRATION))
+                .signWith(secretKey)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return extractClaims(token, Claims::getSubject);
+    }
+
+    private <T> T extractClaims(String token, Function<Claims, T> claimsResolver) {
+        return claimsResolver.apply(Jwts.parser().verifyWith(secretKey).build().parseClaimsJws(token).getPayload());
+    }
+
+    public Boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    public Boolean isTokenExpired(String token) {
+        return extractClaims(token, Claims::getExpiration).before(new Date());
+    }
+}
