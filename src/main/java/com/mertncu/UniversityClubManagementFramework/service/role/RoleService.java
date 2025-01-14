aaa5b89cfddbf09e30d671542da0b3810591b6a9
Yener Mert Uncu <86873433+mertncu@users.noreply.github.com>
2025-01-14 07:13:51 +0300

    Add CRUD Operations for Club Members Role and ClubRole Entity, Service, Repository, Controller

---
+++ src/main/java/com/mertncu/UniversityClubManagementFramework/service/role/RoleService.java
@@ -0,0 +1,32 @@
+package com.mertncu.UniversityClubManagementFramework.service.role;
+
+import com.mertncu.UniversityClubManagementFramework.entity.ClubRole;
+import com.mertncu.UniversityClubManagementFramework.repository.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RoleService {
+    @Autowired
+    private RoleRepository roleRepository;
+
+    public List<ClubRole> getAllRoles() {
+        return roleRepository.findAll();
+    }
+
+    public ClubRole addRole(ClubRole role) {
+        return roleRepository.save(role);
+    }
+
+    public ClubRole updateRole(int roleId, ClubRole roleDetails) {
+        ClubRole role = roleRepository.findById(roleId).orElseThrow(() -> new RuntimeException("Role not found"));
+        role.setRoleName(roleDetails.getRoleName());
+        return roleRepository.save(role);
+    }
+
+    public void deleteRole(int roleId) {
+        roleRepository.deleteById(roleId);
+    }
+}
